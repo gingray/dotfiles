e12fcaf4d23e586465bb2e741f5c235a0ee5bda1
gingray <gingray.dev@gmail.com>
2023-07-10 16:48:01 +0200

    add global gitignore setup command

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 cp -n .ideavimrc $HOME/.ideavimrc
 cp -n karabiner.json $HOME/.config/karabiner/karabiner.json
 cp -n .global_gitignore $HOME/.global_gitignore
+git config --global core.excludesfile $HOME/.global_gitignore
